1b11557a9cd87cd469cc7042bca595f46c740a39
Greg Swallow <gswallow@gmail.com>
2017-07-06 20:03:52 -0400

    Housecleaning.

--- sparkleformation/components/nat_ami.rb
+++
@@ -1,8 +0,0 @@
-SparkleFormation.build do
-  mappings.region_to_nat_ami do
-    set!('us-east-1'.disable_camel!,    :ami => 'ami-b73b63a0') # amzn-ami-hvm-2016.09.0.x86_64-ebs (NOT the nat ami)
-    set!('us-east-2'.disable_camel!,    :ami => 'ami-58277d3d')
-    set!('us-west-1'.disable_camel!,    :ami => 'ami-23e8a343')
-    set!('us-west-2'.disable_camel!,    :ami => 'ami-5ec1673e')
-  end
-end
--- sparkleformation/vpc.rb
+++ sparkleformation/vpc.rb
@@ -1,18 +1,10 @@
 #SparkleFormation.new(:vpc, :provider => :aws).load(:base, :igw, :ssh_key_pair, :nat_ami, :nat_instance_iam).overrides do
-SparkleFormation.new(:vpc, :provider => :aws).load(:base, :igw, :ssh_key_pair).overrides do
+SparkleFormation.new(:vpc, :provider => :aws).load(:base, :igw) do
   description <<EOF
 VPC, including a NAT instance, NAT and private subnet security groups, and an internal hosted DNS zone.
 EOF
 
   ENV['vpc_name'] ||= "#{ENV['org']}-#{ENV['environment']}-#{ENV['AWS_REGION']}-vpc"
-
-  parameters(:allow_ssh_from) do
-    type 'String'
-    allowed_pattern "(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})/(\\d{1,2})"
-    default '127.0.0.1/32'
-    description 'Network to allow SSH from, to NAT instances. Note that the default of 127.0.0.1/32 effectively disables SSH access.'
-    constraint_description 'Must follow IP/mask notation (e.g. 192.168.1.0/24)'
-  end
 
   dynamic!(:vpc, ENV['vpc_name'])
 
